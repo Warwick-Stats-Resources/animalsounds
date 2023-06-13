7de670d1f9d471fdfe2b51b8cbc6169b6227288a
EllaKaye <ella.kaye@gmail.com>
2023-06-13 12:48:53 +0100

    Imports

--- R/animal_sounds.R
+++ R/animal_sounds.R
@@ -10,7 +10,11 @@
 #'
 #' @examples animal_sounds("dog", "woof")
 animal_sounds <- function(animal, sound) {
-  stopifnot(is.character(animal) & length(animal) == 1)
-  stopifnot(is.character(sound) & length(sound) == 1)
+  #stopifnot(is.character(animal) & length(animal) == 1)
+  #stopifnot(is.character(sound) & length(sound) == 1)
+
+  rlang::is_character(animal, 1)
+  rlang::is_character(sound, 1)
+
   paste0("The ", animal, " goes ", sound, "!")
 }
---
+++ R/mypackage-package.R
@@ -0,0 +1,9 @@
+#' @keywords internal
+"_PACKAGE"
+
+## usethis namespace: start
+#' @importFrom purrr keep
+#' @importFrom purrr modify
+#' @importFrom rlang is_character
+## usethis namespace: end
+NULL
