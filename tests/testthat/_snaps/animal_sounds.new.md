79f00068f32af6df64cf7268d5d00d91de3113c9
EllaKaye <ella.kaye@gmail.com>
2023-06-15 20:43:30 +0100

    Add tests

---
+++ tests/testthat/_snaps/animal_sounds.new.md
@@ -0,0 +1,8 @@
+# error message for invalid input
+
+    Code
+      animal_sounds("dog", c("woof", "bow wow wow"))
+    Error <error_not_single_string>
+      `sound` must be a <character> vector of length 1!
+      i It was a character vector of length 2 instead.
+
---
+++ tests/testthat/test-animal_sounds.R
@@ -0,0 +1,18 @@
+test_that("animal_sounds produces expected strings", {
+  dog_woof <- animal_sounds("dog", "woof")
+  expect_equal(dog_woof,
+               "The dog goes woof!")
+})
+
+test_that("handles invalid inputs", {
+  expect_error(animal_sounds("dog", c("woof", "bow wow wow")),
+               class = "error_not_single_string")
+  expect_error(animal_sounds(factor("cat"), "miaow"),
+               class = "error_not_single_string")
+})
+
+
+test_that("error message for invalid input", {
+  expect_snapshot(animal_sounds("dog", c("woof", "bow wow wow")),
+                  error = TRUE)
+})
