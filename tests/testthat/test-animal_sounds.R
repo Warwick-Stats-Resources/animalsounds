a3c658386d9b8832f4b6215bfbb19e7bededf38d
Heather Turner <ht@heatherturner.net>
2024-06-26 15:19:32 +0100

    add test of error with factor input

--- tests/testthat/test-animal_sounds.R
+++ tests/testthat/test-animal_sounds.R
@@ -7,4 +7,6 @@ test_that("animal_sounds produces expected strings", {
 test_that("handles invalid inputs", {
   expect_error(animal_sounds("dog", c("woof", "bow wow wow")),
                class = "error_not_single_string")
+  expect_error(animal_sounds(factor("cat"), "miaow"),
+               class = "error_not_single_string")
 })
